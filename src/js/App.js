d9ea940232b2875f4ae61897812a7d48b1b5e0a4
Manuel Hensler <scipioafricola@gmail.com>
2024-06-04 22:06:53 +0200

    es ertönt was

--- src/js/App.js
+++ src/js/App.js
@@ -1,8 +1,35 @@
-import React from 'react';
+// import React from 'react';
 
-export default function App() {
+// export default function App() {
 
-    return(
-        <h1>Am I Evil?</h1>
-    )
+//     return(
+//         <h1>Am I Evil?</h1>
+//     )
+// }
+
+import React, {useEffect} from "react";
+import * as Tone from 'tone';
+
+function App() {
+
+useEffect(() => {
+    const synth = new Tone.Synth().toDestination();
+
+    const now = Tone.now();
+
+    synth.triggerAttackRelease("C4","8n",now);
+    synth.triggerAttackRelease("E4","19n",now+0.5);
+    synth.triggerAttackRelease("G4","8n",now+1);
+},
+
+[]);
+
+return(
+   <div className="App">
+    <h1>Hello, tone.js</h1>
+   </div>
+);
+
 }
+
+export default App;
